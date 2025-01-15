f5ef91d717ec498978ee36f38c91f3f234c870a6
John Vandervliet <112518201+johnvvandervliet@users.noreply.github.com>
2025-01-15 14:33:18 -0700

    Update terraform_cli_install.sh

--- bin/terraform_cli_install.sh
+++ bin/terraform_cli_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash bash
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint

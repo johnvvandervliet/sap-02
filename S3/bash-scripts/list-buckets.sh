e7063bfbbcfbdc6718f8e47f1a3011c43946ca61
John Vandervliet <johnv.yyc@gmail.com>
2025-01-24 19:36:09 +0000

    latest updates for delete objects

--- S3/bash-scripts/list-buckets.sh
+++ S3/bash-scripts/list-buckets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "== list newest buckets"
 
+# https://jqlang.github.io/jq/manual/
+
 aws s3api list-buckets | jq -r '.Buckets | sort_by(.CreationDate) | reverse | .[0:5] | .[] | .Name'
 echo "..."
